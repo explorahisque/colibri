0c2dea0ea6f829b1f9eb882ebf5b278fb727241a
explorahisque <explora@hisque.com.co>
2025-03-29 13:16:12 -0500

    Resolución de conflictos

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // db.js
 const { Pool } = require('pg');
 
@@ -24,30 +23,3 @@ module.exports = {
   query: (text, params) => pool.query(text, params),
   pool,
 };
-=======
-// db.js
-const { Pool } = require('pg');
-
-// Configura los detalles de conexión
-const pool = new Pool({
-  user: 'exploracolibri',           // Usuario de PostgreSQL (por defecto es 'postgres')
-  host: 'colibridb.postgres.database.azure.com',          // Host local
-  database: 'educacionazure',      // Nombre de la base de datos creada
-  password: 'fHijP7qFB99rr4V',   // Reemplaza con tu contraseña
-  port: 5432,                 // Puerto por defecto
-  ssl: {
-    require: true, // Forzar SSL
-    rejectUnauthorized: false // Opcional: dependiendo de la configuración del certificado en Azure
-  }
-});
-
-// Evento para confirmar la conexión
-pool.on('connect', () => {
-  console.log('Conectado a la base de datos PostgreSQL');
-});
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-  pool,
-};
->>>>>>> 908c2ed0a89214aafe48e1a974e33bb4aa476dce
